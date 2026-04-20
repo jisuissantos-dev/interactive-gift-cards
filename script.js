99e3d087cdd10e89362f1c289a269bb539a8f306
jisuissantos-dev <jisuissantos@gmail.com>
2026-04-20 18:32:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', () => {
+    const duration = 5 * 1000; // duração em ms (5 segundos)
+    const end = Date.now() + duration;
+
+    const interval = setInterval(() => {
+        if (Date.now() > end) {
+            clearInterval(interval);
+            return;
+        }
+
+        confetti({
+            particleCount: 50,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 }, // chuva vindo da esquerda
+            colors: ['#470391', '#d1acf6', '#ffffff', '#ff69b4']
+        });
+
+        confetti({
+            particleCount: 50,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 }, // chuva vindo da direita
+            colors: ['#470391', '#d1acf6', '#ffffff', '#ff69b4']
+        });
+
+    }, 250);
+});
+
+document.querySelectorAll('.vale').forEach(vale => {
+    vale.addEventListener('click', () => {
+        vale.classList.toggle('virado');
+    });
+});
